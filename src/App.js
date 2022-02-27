5bf887b717485c54073a05fe49eacabacf6b322c
Aleksandr <it.kucherov@gmail.com>
2022-02-27 16:38:50 +0300

    lesson of componets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import { Component, Fragment } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const EmpItem = styled.div`
+	padding: 20px;
+	margin-bottom: 15px;
+	border-radius: 5px;
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+	a {
+		display: block;
+		margin: 10px 0 10px 0;
+		color: ${(props) => (props.active ? 'orange' : 'black')};
+	}
+	input {
+		display: black;
+		margin-top: 10px;
+	}
+`;
+
+const Header = styled.h2`
+	font-size: 22px;
+`;
+
+export const Button = styled.button`
+	display: block;
+	padding: 5px 15px;
+	background-color: gold;
+	border: 1px solid rgba(0, 0, 0, 0.2);
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
 
 class WhoAmI extends Component {
 	constructor(props) {
@@ -7,6 +36,7 @@ class WhoAmI extends Component {
 		this.state = {
 			years: 27,
 			position: '',
+			text: 'KNOPKA',
 		};
 	}
 
@@ -25,12 +55,12 @@ class WhoAmI extends Component {
 		const { name, surname, link } = this.props;
 		const { position, years } = this.state;
 		return (
-			<>
-				<button onClick={this.nextYear}>{this.state.text}</button>
-				<h1>
+			<EmpItem active>
+				<Button onClick={this.nextYear}>{this.state.text}</Button>
+				<Header>
 					My name is {name}, surname - {surname}, age - {years}, position -{' '}
 					{position}
-				</h1>
+				</Header>
 				<a href={link}>My profile</a>
 				<form>
 					<span>Введите должность</span>
@@ -39,17 +69,22 @@ class WhoAmI extends Component {
 						onChange={(e) => this.commitInputChanges(e, 'some color')}
 					/>
 				</form>
-			</>
+			</EmpItem>
 		);
 	}
 }
 
+const Wrapper = styled.div`
+	width: 600px;
+	margin: 80px auto 0 auto;
+`;
+
 function App() {
 	return (
-		<div className="App">
+		<Wrapper>
 			<WhoAmI name="John" surname="Smith" link="facebook.com" />
 			<WhoAmI name="Alex" surname="Shepard" link="vk.com" />
-		</div>
+		</Wrapper>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { Button } from './App';
+import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootstrapTest from './BootstrapTest';
+
+const BigButton = styled(Button)`
+	margin: 0 auto;
+	width: 245px;
+	text-align: center;
+`;
+
 ReactDOM.render(
 	<StrictMode>
 		<App />
+		<BigButton as="a">BIGKNOPKA</BigButton>
+		<BootstrapTest />
 	</StrictMode>,
 	document.getElementById('root')
 );
